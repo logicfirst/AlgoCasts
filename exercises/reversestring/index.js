7d55d67b69eaae23da01515038e23df360083308
logicfirst <slee@logicfirstllc.com>
2021-04-14 15:33:50 -0400

    scott-0414-1530-worked on reverse string problem

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,32 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  let reversed = ''
+
+  for (var i=0; i<str.length; i++){
+    
+  }
+}
+// Solution1
+// function reverse(str) {
+//   const arr = str.split('')
+//   arr.reverse()
+//   return arr.join('')
+// }
+// Solution1b
+//  return str.split('').reverse().join('')
+
+// Solution2
+// function reverse(str) {
+//   let reversed = ''
+
+//   for (let character of str){
+//     reversed = character + reversed
+//   }
+//   return reversed
+// }
+
+
 
 module.exports = reverse;
